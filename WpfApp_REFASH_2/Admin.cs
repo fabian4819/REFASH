bd355070c812753c8237b49534dc29ec3dd9a34d
fabian4819 <habibfabianfahlesi2004@mail.ugm.ac.id>
2024-09-03 14:11:08 +0700

    feat: object and function for Admin.cs

--- WpfApp_REFASH_2/Admin.cs
+++ WpfApp_REFASH_2/Admin.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Animation;
 
 namespace WpfApp_REFASH
 {
     internal class Admin
     {
+        public string adminID { get; set; }
+        public int totalContent { get; set; }
+        public void addContent(string title, byte[] image, string description)
+        {
+            totalContent++;
+        }   
+        public void removeContent(string contentID)
+        {
+            totalContent--;
+        }
+        public void updateContent(string contentID, string title, byte[] image, string description)
+        {
+            // Update content
+        }
+        public void verifyCollection(string collectionID)
+        {
+            // Verify collection
+        }
+        public void addProduct(string name, byte[] image, string price, string category, string size, int stock)
+        {
+            // Add product
+        }
+        public void removeProduct(string productID)
+        {
+            // Remove product
+        }
+        public void updateProduct(string productID, string name, byte[] image, string price, string category, string size, int stock)
+        {
+            // Update product
+        }
     }
 }
