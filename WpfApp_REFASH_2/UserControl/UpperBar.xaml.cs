d4cb6062e218adb9ace122fd6e8149ed525bfe48
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-11-06 17:35:08 +0700

    Refactor: simplified sidebar and upperbar into user control

---
+++ WpfApp_REFASH_2/UserControl/UpperBar.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_REFASH
+{
+    /// <summary>
+    /// Interaction logic for UpperBar.xaml
+    /// </summary>
+    public partial class UpperBar : UserControl
+    {
+        public UpperBar()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_minimizeApp_click(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+            parentWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void btn_closeApp_click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
