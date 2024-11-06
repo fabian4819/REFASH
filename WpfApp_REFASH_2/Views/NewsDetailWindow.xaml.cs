18c4f48ec380a592a08b84f7fa2fb350e04d39c4
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-11-06 16:20:15 +0700

    Feat: News Detail Window for reading content full version

---
+++ WpfApp_REFASH_2/Views/NewsDetailWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp_REFASH
+{
+    /// <summary>
+    /// Interaction logic for NewsDetailWindow.xaml
+    /// </summary>
+    public partial class NewsDetailWindow : Window
+    {
+        public NewsDetailWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_closeApp_click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btn_minimizeApp_click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btn_back_click(object sender, RoutedEventArgs e)
+        {
+            NewsWindow newsWindow = new NewsWindow();
+            newsWindow.Show();
+            this.Close();
+        }
+
+        private void btn_navigateNews_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
