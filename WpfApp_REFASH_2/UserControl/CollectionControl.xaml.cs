642e35a94aadc2d8fec71aaeb851204d8a169ae8
fabian4819 <habibfabianfahlesi2004@mail.ugm.ac.id>
2024-11-07 15:35:27 +0700

    fix eror collection

--- WpfApp_REFASH_2/UserControl/CollectionControl.xaml.cs
+++ WpfApp_REFASH_2/UserControl/CollectionControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp_REFASH.Views
+namespace WpfApp_REFASH
 {
     public partial class CollectionControl : UserControl
     {
