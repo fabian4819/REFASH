1dc4f6c5b4f66608f0e57b589761bff032b933b7
fabian4819 <habibfabianfahlesi2004@mail.ugm.ac.id>
2024-11-18 02:50:01 +0700

    fix: style for news

---
+++ WpfApp_REFASH_2/Utilities/ImageHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp_REFASH
+{
+    public static class ImageHelper
+    {
+        public static ImageSource ByteArrayToImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0)
+                return null;
+
+            try
+            {
+                var image = new BitmapImage();
+                using (var mem = new MemoryStream(imageData))
+                {
+                    mem.Position = 0;
+                    image.BeginInit();
+                    image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.StreamSource = mem;
+                    image.EndInit();
+                }
+                image.Freeze();
+                return image;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static byte[] GetImageBytes(string imagePath)
+        {
+            if (string.IsNullOrEmpty(imagePath) || !File.Exists(imagePath))
+                return null;
+
+            try
+            {
+                return File.ReadAllBytes(imagePath);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
