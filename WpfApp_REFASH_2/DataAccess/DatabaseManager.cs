bbaa1c41811d2b1a653de40ae5983ef38bbfaa49
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-11-23 17:32:29 +0700

    Update: Readme

--- WpfApp_REFASH_2/DataAccess/DatabaseManager.cs
+++ WpfApp_REFASH_2/DataAccess/DatabaseManager.cs
@@ -16,8 +16,7 @@ namespace WpfApp_REFASH.DataAccess
 
     public DatabaseManager()
     {
-        ConnectionString = Environment.GetEnvironmentVariable("REFASH_CONNECTION_STRING")
-                           ?? throw new InvalidOperationException("Connection string not found in environment variables.");
+            ConnectionString = "Host=localhost;Database=junpro100;Username=postgres;Password=12345678";
     }
 
     public NpgsqlConnection GetConnection()
